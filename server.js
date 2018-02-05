072f3628bdc4826677ed0cbe22ef1901d0388db5
Will <william.altxx@gmail.com>
2018-02-05 13:39:28 -0600

    mongodbURI troubleshooting continued

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 
 mongoose.connect(
-  "mongodb://heroku_3f6zqnm3:hsggctnqdri5rjl187342n8vfp@ds225078.mlab.com:25078/heroku_3f6zqnm3" || "mongodb://localhost/ispyourself",
+  "mongodb://heroku_3f6zqnm3:hsggctnqdri5rjl187342n8vfp@ds225078.mlab.com:25078/heroku_3f6zqnm3",
   {
     useMongoClient: true
   }
