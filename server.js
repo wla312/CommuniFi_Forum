b8c802b22d71dcefeeef1dc49ac41bf4db318d0b
Will <william.altxx@gmail.com>
2018-02-05 14:34:33 -0600

    added cors pkg per stackoverflow

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+const cors = require("cors");
+
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
