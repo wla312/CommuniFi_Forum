913c352df52eef989ec35c7da51183c6b3e9321e
Will <william.altxx@gmail.com>
2018-02-05 11:53:18 -0600

    first commit to new repo

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// adding registerServiceWorker to see if it solves deployment issue...
+import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
