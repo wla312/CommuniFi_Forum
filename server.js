843838922f16f91c8fb725ed310b4cc88aa5b21b
Will <william.altxx@gmail.com>
2018-02-06 12:40:55 -0600

    hardcode URI in server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/ispyourself",
+  "mongodb://heroku_3f6zqnm3:hsggctnqdri5rjl187342n8vfp@ds225078.mlab.com:25078/heroku_3f6zqnm3",
   {
     useMongoClient: true
   }
